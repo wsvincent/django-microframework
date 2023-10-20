5eec7e7acf6ca767740f2c5fcb03710cd1b6c267
programmylife <acmshar@gmail.com>
2023-10-20 10:44:55 -0400

    remove hello_django1 and update hello_django.py

--- hello_django.py
+++ hello_django.py
@@ -1,17 +1,22 @@
 from django.conf import settings
 from django.core.handlers.wsgi import WSGIHandler
+from django.core.management import execute_from_command_line
 from django.http import HttpResponse
 from django.urls import path
 
 settings.configure(
     ROOT_URLCONF=__name__,
+    DEBUG=True,
 )
+
 
 def hello_world(request):
     return HttpResponse("Hello, Django!")
 
-urlpatterns = [
-    path("", hello_world)
-]
+
+urlpatterns = [path("hello-world", hello_world)]
 
 application = WSGIHandler()
+
+if __name__ == "__main__":
+    execute_from_command_line()
